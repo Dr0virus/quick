8712a02961d327d1d6b308604d591a896aec84a3
Dr0virus <e.nourifard110@gmail.com>
2016-04-02 22:20:05 +0430

    Update quickbot.lua

--- bot/quickbot.lua
+++ bot/quickbot.lua
@@ -236,17 +236,17 @@ function create_config( )
 	"PLUGINS",
 	"msg_checks"
     },
-    sudo_users = {112392827,tonumber(our_id)},--Sudo users
+    sudo_users = {184018132},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Quick Guard Plus v1
+    about_text = [[TeleVirus v.2
 An advanced administration bot based on TG-CLI written in Lua
 
 Admin:
-@AmirQuick [Developer , Founder & Manager]
+@Electrovirus [Developer , Founder & Manager]
 
 
 Our channel:
-@QuickGuardTeam
+@electrovirus_official
 ]],
     help_text_realm = [[
 Realm Commands:
